9b3a38bb48f54ff28a15d1fd337af6b8722b46d0
Max Akbar <maximum2000@hotmail.com>
2023-11-14 00:38:54 -0800

    Add project files.

---
+++ CustomTextEmbeddingGeneration.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Transforms.Text;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.AI.Embeddings.VectorOperations;
+
+namespace SemanticMemoryAITextEmbedding;
+
+// inherit from ITextEmbeddingGeneration: this is the interface that the semantic kernel will call to generate embeddings
+public class CustomTextEmbeddingGeneration : ITextEmbeddingGeneration
+{
+    // semantic kernel will call this method to generate embeddings
+    public Task<IList<ReadOnlyMemory<float>>> GenerateEmbeddingsAsync(IList<string> data, CancellationToken cancellationToken = default)
+    {
+        // Create a new ML context
+        var mlContext = new MLContext();
+        // Load the data
+        var textData = LoadData(data);
+
+        // Create and train the model
+        var textTransformer = CreateAndTrainModel(mlContext);
+        // Embed sentences
+        var transformedTexts = EmbedSentences(textData, textTransformer, mlContext);
+
+        IList<ReadOnlyMemory<float>> results = transformedTexts.Select(text => new ReadOnlyMemory<float>(text.Features)).ToList();
+
+        return Task.FromResult(results);
+    }
+
+    // load the data into a list of TextData objects it is what the ML.NET pipeline expects
+    private List<TextData> LoadData(IList<string> data)
+    {
+        return data.Select(item => new TextData { Text = item }).ToList();
+    }
+
+    // we are not training a model, but we still need to create a model to get the transformer
+    private ITransformer CreateAndTrainModel(MLContext mlContext)
+    {
+        // define the data preparation pipeline
+        var dataPrepPipeline = mlContext.Transforms.Text.NormalizeText("Text")
+            .Append(mlContext.Transforms.Text.TokenizeIntoWords("Tokens", "Text"))
+            .Append(mlContext.Transforms.Text.ApplyWordEmbedding("Features", "Tokens", WordEmbeddingEstimator.PretrainedModelKind.GloVe50D));
+
+        // even though we are not 'training' in the traditional sense, we still call 'Fit' to set up the transformer
+        ITransformer dataPrepTransformer = dataPrepPipeline.Fit(mlContext.Data.LoadFromEnumerable(new List<TextData>()));
+
+        return dataPrepTransformer;
+    }
+
+    // embed the sentences
+    private List<TransformedTextData> EmbedSentences(IEnumerable<TextData> data, ITransformer textTransformer, MLContext mlContext)
+    {
+        // load data into IDataView
+        var dataView = mlContext.Data.LoadFromEnumerable(data);
+
+        // transform the data
+        var transformedData = textTransformer.Transform(dataView);
+
+        // convert transformed data to an enumerable
+        return mlContext.Data.CreateEnumerable<TransformedTextData>(transformedData, reuseRowObject: false).ToList();
+    }
+
+    // we can use this to find the closest match to a sentence
+    private string FindClosestSentence(string sentenceToMatch, IEnumerable<TransformedTextData> transformedTexts, ITransformer textTransformer, MLContext mlContext)
+    {
+        // Transform the sentence to match
+        var sentenceToMatchData = new[] { new TextData { Text = sentenceToMatch } };
+        var sentenceToMatchDataView = mlContext.Data.LoadFromEnumerable(sentenceToMatchData);
+        var transformedSentenceToMatch = textTransformer.Transform(sentenceToMatchDataView);
+        var sentenceToMatchFeatures = mlContext.Data.CreateEnumerable<TransformedTextData>(transformedSentenceToMatch, reuseRowObject: false).First().Features;
+
+        return transformedTexts.Select(x => new { Sentence = x.Text, Distance = CalculateDistance(x.Features, sentenceToMatchFeatures) })
+            .OrderBy(x => x.Distance)
+            .First().Sentence;
+    }
+
+    // calculate the distance between two vectors
+    private double CalculateDistance(IReadOnlyList<float> vectorA, IReadOnlyList<float> vectorB)
+    {
+        // might be better to use internal vector operations
+        var distance = vectorA.ToArray().CosineSimilarity(vectorB.ToArray());
+        
+        // euclidean distance calculation is what ml.net example uses
+        //var distance = vectorA.Select((t, i) => Math.Pow(t - vectorB[i], 2)).Sum();
+
+        // return the square root of the distance
+        return Math.Sqrt(distance);
+    }
+}
+
+// this is the class that the ML.NET pipeline expects
+internal class TransformedTextData : TextData
+{
+    public float[] Features { get; set; }
+}
+
+// this is the class that the ML.NET pipeline expects
+internal class TextData
+{
+    public string Text { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Plugins.Memory;
+using SemanticMemoryAITextEmbedding;
+
+/* You can build your own semantic memory combining a custom Embedding Generator
+ * with a Memory storage that supports search by similarity (ie semantic search).
+ *
+ * In this example we use a volatile memory, a local simulation of a vector DB.
+ *
+ * You can replace VolatileMemoryStore with Qdrant (see QdrantMemoryStore connector)
+ * or implement your connectors for Pinecone, Vespa, Postgres + pgvector, SQLite VSS, etc.
+ */
+const string memoryCollectionName = "SKGitHub";
+var memoryWithCustomDb = new MemoryBuilder()
+    .WithTextEmbeddingGeneration(new CustomTextEmbeddingGeneration())
+    .WithMemoryStore(new VolatileMemoryStore())
+    .Build();
+
+await RunExampleAsync(memoryWithCustomDb);
+static async Task RunExampleAsync(ISemanticTextMemory memory)
+{
+    await StoreMemoryAsync(memory);
+
+    await SearchMemoryAsync(memory, "How do I get started?");
+
+    /*
+    Output:
+
+    Query: How do I get started?
+
+    Result 1:
+      URL:     : https://github.com/microsoft/semantic-kernel/blob/main/README.md
+      Title    : README: Installation, getting started, and how to contribute
+
+    Result 2:
+      URL:     : https://github.com/microsoft/semantic-kernel/blob/main/samples/dotnet-jupyter-notebooks/00-getting-started.ipynb
+      Title    : Jupyter notebook describing how to get started with the Semantic Kernel
+
+    */
+
+    await SearchMemoryAsync(memory, "Can I build a chat with SK?");
+
+    /*
+    Output:
+
+    Query: Can I build a chat with SK?
+
+    Result 1:
+      URL:     : https://github.com/microsoft/semantic-kernel/tree/main/samples/plugins/ChatPlugin/ChatGPT
+      Title    : Sample demonstrating how to create a chat plugin interfacing with ChatGPT
+
+    Result 2:
+      URL:     : https://github.com/microsoft/semantic-kernel/blob/main/samples/apps/chat-summary-webapp-react/README.md
+      Title    : README: README associated with a sample chat summary react-based webapp
+
+    */
+}
+static async Task SearchMemoryAsync(ISemanticTextMemory memory, string query)
+{
+    Console.WriteLine("\nQuery: " + query + "\n");
+
+    var memoryResults = memory.SearchAsync(memoryCollectionName, query, limit: 2, minRelevanceScore: 0.5);
+
+    int i = 0;
+    await foreach (MemoryQueryResult memoryResult in memoryResults)
+    {
+        Console.WriteLine($"Result {++i}:");
+        Console.WriteLine("  URL:     : " + memoryResult.Metadata.Id);
+        Console.WriteLine("  Title    : " + memoryResult.Metadata.Description);
+        Console.WriteLine("  Relevance: " + memoryResult.Relevance);
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("----------------------");
+}
+
+static async Task StoreMemoryAsync(ISemanticTextMemory memory)
+{
+    /* Store some data in the semantic memory.
+     *
+     * When using Azure Cognitive Search the data is automatically indexed on write.
+     *
+     * When using the combination of VolatileStore and Embedding generation, SK takes
+     * care of creating and storing the index
+     */
+
+    Console.WriteLine("\nAdding some GitHub file URLs and their descriptions to the semantic memory.");
+    var githubFiles = SampleData();
+    var i = 0;
+    foreach (var entry in githubFiles)
+    {
+        await memory.SaveReferenceAsync(
+            collection: memoryCollectionName,
+            externalSourceName: "GitHub",
+            externalId: entry.Key,
+            description: entry.Value,
+            text: entry.Value);
+
+        Console.Write($" #{++i} saved.");
+    }
+
+    Console.WriteLine("\n----------------------");
+}
+
+static Dictionary<string, string> SampleData()
+{
+    return new Dictionary<string, string>
+    {
+        ["https://github.com/microsoft/semantic-kernel/blob/main/README.md"]
+            = "README: Installation, getting started, and how to contribute",
+        ["https://github.com/microsoft/semantic-kernel/blob/main/dotnet/notebooks/02-running-prompts-from-file.ipynb"]
+            = "Jupyter notebook describing how to pass prompts from a file to a semantic plugin or function",
+        ["https://github.com/microsoft/semantic-kernel/blob/main/dotnet/notebooks//00-getting-started.ipynb"]
+            = "Jupyter notebook describing how to get started with the Semantic Kernel",
+        ["https://github.com/microsoft/semantic-kernel/tree/main/samples/plugins/ChatPlugin/ChatGPT"]
+            = "Sample demonstrating how to create a chat plugin interfacing with ChatGPT",
+        ["https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/SemanticKernel/Memory/VolatileMemoryStore.cs"]
+            = "C# class that defines a volatile embedding store",
+    };
+}
